b942d38ec032c806a9515e31eda77501d1b81594
Евгений <lovepsone@mail.ru>
2014-12-03 20:50:58 +0300

    [073] code opimization

--- include/js/DragAndDrop.js
+++ include/js/DragAndDrop.js
@@ -1,7 +1,7 @@
 /**
  * @package Survarium Armory
  * @version Release 1.2
- * @revision 71
+ * @revision 73
  * @copyright (c) 2014 lovepsone
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  *
@@ -26,127 +26,57 @@ function HeadCheckSlots2(id)
 		return true;
 	return false;
 }
+function AddDraggable(Selector, TypeItem, ClassTooltip)
+{
+	$(Selector).droppable(
+	{
+		tolerance:'touch',
+		accept:'#'+TypeItem,
+		drop:function (event, ui)
+		{
+			var id = $(this).attr("id"), move = ui.draggable, idItem = GetId(move.find("div[item-id]").html()), img = GetImg(move.find("div[item-id]").html());
+			$('div.last' + id).empty();
+			if (TypeItem == 'im')
+			{
+				$("div.lastSelectHead2").empty();
+				$("div.lastSelectMask2").empty();
+			}
+			if (TypeItem == 'ie')
+			{
+				$("div.lastSelectHead2").empty();
+				$("div.lastSelectMask2").empty();
+				if (HeadCheckSlots2(idItem))
+				{
+					$("div.lastSelectMask").empty();
+					$(this).append('<div iteml-id="'+move.attr("item-id")+'" class="lastSelectHead2"><img src="images/icon/'+img+'.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="'+ClassTooltip+'"/></div>');
+					$("#SelectMask").append('<div iteml-id="'+move.attr("item-id")+'" class="lastSelectMask2"><img src="images/icon/'+img+'_.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="'+ClassTooltip+'"/></div>');
+					$("img."+ClassTooltip).easyTooltip({tooltipId: "TooltipItemIcon"});
+				}
+				else
+				{
+					$(this).append('<div iteml-id="'+move.attr("item-id")+'" class="last'+id+'"><img src="images/icon/'+img+'.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="'+ClassTooltip+'"/></div>');
+					$("img."+ClassTooltip).easyTooltip({tooltipId: "TooltipItemIcon"});
+				}
+			}
+			else
+			{
+				$(this).append('<div iteml-id="'+move.attr("div[item-id]")+'" class="last'+id+'"><img src="images/icon/'+img+'.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="'+ClassTooltip+'"/></div>');
+				$("img."+ClassTooltip).easyTooltip({tooltipId: "TooltipItemIcon"});
+			}
+			$.ajax({url: 'include/HandleArmory.php',type: 'POST',data:{'data':TypeItem+':'+idItem+':0'},success: function(data){$("#StatsOutput").html(data);$("img.BonusTT").easyTooltip({tooltipId: "TooltipItemIcon"});}});
+		}
+	});
+}
 
 function updateDraggable()
 {
 	$("div #iw, div #ie, div #im, div #ia, div #ib, div #ih, div #is, div #if").draggable({helper:'clone',zIndex: 9999,start: function(event, ui){$(this).data('preventBehaviour', true);}});
-	$("#SelectWeapon").droppable(
-	{
-		tolerance:"touch",
-		accept:"#iw",
-		drop:function (event, ui)
-		{
-			var id = $(this).attr("id"), move = ui.draggable, idItem = GetId(move.find("div[item-id]").html());
-			$("div.last" + id).empty();
-			$(this).append('<div iteml-id="'+move.attr("div[item-id]")+'" class="last'+id+'"><img src="images/icon/'+GetImg(move.find("div[item-id]").html())+'.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="ItemW"/></div>');
-			$("img.ItemW").easyTooltip({tooltipId: "TooltipItemIcon"});
-			$.ajax({url: 'include/HandleArmory.php',type: 'POST',data:{'data':'iw:'+idItem+':0'},success: function(data){$("#StatsOutput").html(data);$("img.BonusTT").easyTooltip({tooltipId: "TooltipItemIcon"});}});
-		}
-	});
-	$("#SelectHead").droppable(
-	{
-		tolerance:"touch",
-		accept:"#ie",
-		drop:function (event, ui)
-		{
-			var id = $(this).attr("id"), move = ui.draggable, idItem = GetId(move.find("div[item-id]").html());
-			$("div.last" + id).empty();
-			$("div.lastSelectHead2").empty();
-			$("div.lastSelectMask2").empty();
-			if (HeadCheckSlots2(idItem))
-			{
-				$("div.lastSelectMask").empty();
-				$(this).append('<div iteml-id="'+move.attr("item-id")+'" class="lastSelectHead2"><img src="images/icon/'+GetImg(move.find("div[item-id]").html())+'.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="ItemE"/></div>');
-				$("#SelectMask").append('<div iteml-id="'+move.attr("item-id")+'" class="lastSelectMask2"><img src="images/icon/'+GetImg(move.find("div[item-id]").html())+'_.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="ItemE"/></div>');
-				$("img.ItemE").easyTooltip({tooltipId: "TooltipItemIcon"});
-			}
-			else
-			{
-				$(this).append('<div iteml-id="'+move.attr("item-id")+'" class="last'+id+'"><img src="images/icon/'+GetImg(move.find("div[item-id]").html())+'.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="ItemE"/></div>');
-				$("img.ItemE").easyTooltip({tooltipId: "TooltipItemIcon"});
-			}
-			$.ajax({url: 'include/HandleArmory.php',type: 'POST',data:{'data':'ie:'+idItem+':0'},success: function(data){$("#StatsOutput").html(data);$("img.BonusTT").easyTooltip({tooltipId: "TooltipItemIcon"});}});
-		}
-	});
-	$("#SelectMask").droppable(
-	{
-		tolerance:"touch",
-		accept:"#im",
-		drop:function (event, ui)
-		{
-			var id = $(this).attr("id"), move = ui.draggable, idItem = GetId(move.find("div[item-id]").html());
-			$("div.lastSelectHead2").empty();
-			$("div.lastSelectMask2").empty();
-			$("div.last" + id).empty();
-			$(this).append('<div iteml-id="'+move.attr("item-id")+'" class="last'+id+'"><img src="images/icon/'+GetImg(move.find("div[item-id]").html())+'.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="ItemM"/></div>');
-			$("img.ItemM").easyTooltip({tooltipId: "TooltipItemIcon"});
-			$.ajax({url: 'include/HandleArmory.php',type: 'POST',data:{'data':'im:'+idItem+':0'},success: function(data){$("#StatsOutput").html(data);$("img.BonusTT").easyTooltip({tooltipId: "TooltipItemIcon"});}});
-		}
-	});
-	$("#SelectBack").droppable(
-	{
-		tolerance:"touch",
-		accept:"#ib",
-		drop:function (event, ui)
-		{
-			var id = $(this).attr("id"), move = ui.draggable, idItem = GetId(move.find("div[item-id]").html());
-			$("div.last" + id).empty();
-			$(this).append('<div iteml-id="'+move.attr("item-id")+'" class="last'+id+'"><img src="images/icon/'+GetImg(move.find("div[item-id]").html())+'.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="ItemB"/></div>');
-			$("img.ItemB").easyTooltip({tooltipId: "TooltipItemIcon"});
-			$.ajax({url: 'include/HandleArmory.php',type: 'POST',data:{'data':'ib:'+idItem+':0'},success: function(data){$("#StatsOutput").html(data);$("img.BonusTT").easyTooltip({tooltipId: "TooltipItemIcon"});}});
-		}
-	});
-	$("#SelectArmory").droppable(
-	{
-		tolerance:"touch",
-		accept:"#ia",
-		drop:function (event, ui)
-		{
-			var id = $(this).attr("id"), move = ui.draggable, idItem = GetId(move.find("div[item-id]").html());
-			$("div.last" + id).empty();
-			$(this).append('<div iteml-id="'+move.attr("item-id")+'" class="last'+id+'"><img src="images/icon/'+GetImg(move.find("div[item-id]").html())+'.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="ItemA"/></div>');
-			$("img.ItemA").easyTooltip({tooltipId: "TooltipItemIcon"});
-			$.ajax({url: 'include/HandleArmory.php',type: 'POST',data:{'data':'ia:'+idItem+':0'},success: function(data){$("#StatsOutput").html(data);$("img.BonusTT").easyTooltip({tooltipId: "TooltipItemIcon"});}});
-		}
-	});
-	$("#SelectHand").droppable(
-	{
-		tolerance:"touch",
-		accept:"#ih",
-		drop:function (event, ui)
-		{
-			var id = $(this).attr("id"), move = ui.draggable, idItem = GetId(move.find("div[item-id]").html());
-			$("div.last" + id).empty();
-			$(this).append('<div iteml-id="'+move.attr("item-id")+'" class="last'+id+'"><img src="images/icon/'+GetImg(move.find("div[item-id]").html())+'.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="ItemH"/></div>');
-			$("img.ItemH").easyTooltip({tooltipId: "TooltipItemIcon"});
-			$.ajax({url: 'include/HandleArmory.php',type: 'POST',data:{'data':'ih:'+idItem+':0'},success: function(data){$("#StatsOutput").html(data);$("img.BonusTT").easyTooltip({tooltipId: "TooltipItemIcon"});}});
-		}
-	});
-	$("#SelectShin").droppable(
-	{
-		tolerance:"touch",
-		accept:"#is",
-		drop:function (event, ui)
-		{
-			var id = $(this).attr("id"), move = ui.draggable, idItem = GetId(move.find("div[item-id]").html());
-			$("div.last" + id).empty();
-			$(this).append('<div iteml-id="'+move.attr("item-id")+'" class="last'+id+'"><img src="images/icon/'+GetImg(move.find("div[item-id]").html())+'.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="ItemS"/></div>');
-			$("img.ItemS").easyTooltip({tooltipId: "TooltipItemIcon"});
-			$.ajax({url: 'include/HandleArmory.php',type: 'POST',data:{'data':'is:'+idItem+':0'},success: function(data){$("#StatsOutput").html(data);$("img.BonusTT").easyTooltip({tooltipId: "TooltipItemIcon"});}});
-		
-		}
-	});
-	$("#SelectFeet").droppable(
-	{
-		tolerance:"touch",
-		accept:"#if",
-		drop:function (event, ui)
-		{
-			var id = $(this).attr("id"), move = ui.draggable, idItem = GetId(move.find("div[item-id]").html());
-			$("div.last" + id).empty();
-			$(this).append('<div iteml-id="'+move.attr("item-id")+'" class="last'+id+'"><img src="images/icon/'+GetImg(move.find("div[item-id]").html())+'.png" id="'+idItem+'" title="'+GetSrc(move.find("div[item-id]").html(), 3)+ '" class="ItemF"/></div>');
-			$("img.ItemF").easyTooltip({tooltipId: "TooltipItemIcon"});
-			$.ajax({url: 'include/HandleArmory.php',type: 'POST',data:{'data':'if:'+idItem+':0'},success: function(data){$("#StatsOutput").html(data);$("img.BonusTT").easyTooltip({tooltipId: "TooltipItemIcon"});}});
-		}
-	});
+	AddDraggable('#SelectWeapon', 'iw', "ItemW");
+	AddDraggable('#SelectHead', 'ie', "ItemE");
+	AddDraggable('#SelectMask', 'im', "ItemM");
+	AddDraggable('#SelectBack', 'ib', "ItemB");
+	AddDraggable('#SelectArmory', 'ia', "ItemA");
+	AddDraggable('#SelectHand', 'ih', "ItemH");
+	AddDraggable('#SelectShin', 'is', "ItemS");
+	AddDraggable('#SelectFeet', 'if', "ItemF");
 }
